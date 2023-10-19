0c4ebeeafd96d2ecd671ccc94e56fafb593ec207
Poornachandraprasad Kongara <kongara.p@northeastern.edu>
2023-10-19 14:33:30 -0400

    change in packer issue

--- routes/assignmentRoutes.js
+++ routes/assignmentRoutes.js
@@ -4,7 +4,7 @@ const { getAssignments,
     getAssignmentById,
     createAssignment,
     updateAssignment,
-    deleteAssignment } = require('../controllers/assignmentController');
+    deleteAssignment } = require('../controllers/Assignment');
 
 const router = express.Router();
 
